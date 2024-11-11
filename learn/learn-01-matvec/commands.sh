a5a2020441f4fa4d5a206f671283a4db8355d157
Sameeran Joshi <joshisameeran17@gmail.com>
2024-11-11 19:35:38 +0000

    Gemv tut 6 routing works, added DSD operations

--- learn/learn-01-matvec/commands.sh
+++ learn/learn-01-matvec/commands.sh
@@ -3,7 +3,7 @@
 set -e  # Exit on first error
 
 echo "Compiling layout.csl to create out ELF file..."
-cslc layout.csl --fabric-dims=11,3 --fabric-offsets=4,1 --params=M:4,N:6,width:4 --memcpy --channels=1 -o out
+cslc layout.csl --fabric-dims=9,3 --fabric-offsets=4,1 --params=M:4,N:6,width:2 --memcpy --channels=1 -o out
 
 echo "Running the host-device interaction through run.py..."
 cs_python run.py --name out/
--- learn/learn-01-matvec/run.py
+++ learn/learn-01-matvec/run.py
@@ -29,11 +29,13 @@ N = int(params['N'])
 width = int(params['width'])
 
 # host A, x, b, y
-host_A = np.arange(M*N, dtype=np.float32)
-host_x = np.full(shape=N, fill_value=2.0, dtype=np.float32)
+host_A = np.arange(M*N, dtype=np.float32).reshape(M,N)
+host_x = np.full(shape=N, fill_value=1.0, dtype=np.float32)
 host_b = np.full(shape=M, fill_value=2.0, dtype=np.float32)
 
 
+host_y_expected = host_A@host_x + host_b
+N_per_PE = N // width
 
 # Initialize the SdkRuntime runner
 runner = SdkRuntime(args.name, cmaddr=args.cmaddr)
@@ -41,27 +43,27 @@ runner = SdkRuntime(args.name, cmaddr=args.cmaddr)
 device_data_y = runner.get_id('y_advertised')
 device_data_A = runner.get_id('A_advertised')
 device_data_x = runner.get_id('x_advertised')
-device_data_b = runner.get_id('b_advertised')
+# device_data_b = runner.get_id('b_advertised')
 
 
 runner.load()
 runner.run()
 #copy from host to device for A
-runner.memcpy_h2d(device_data_A, np.tile(host_A, width), 0, 0, width, 1, M*N, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+runner.memcpy_h2d(device_data_A, host_A.transpose().ravel(), 0, 0, width, 1, M*N_per_PE, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
 #copy from host to device for x
-runner.memcpy_h2d(device_data_x, np.tile(host_x, width), 0, 0, width, 1, N, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+runner.memcpy_h2d(device_data_x, host_x, 0, 0, width, 1, N_per_PE, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
 #copy from host to device for b
-runner.memcpy_h2d(device_data_b, np.tile(host_b, width), 0, 0, width, 1, M, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+# explicitely on 1st PE
+runner.memcpy_h2d(device_data_y, host_b, 0, 0, 1, 1, M, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
 
 runner.launch('init_and_compute_advertised', nonblock=False)
 
-y_returned = np.zeros([M*width], dtype=np.float32)
-runner.memcpy_d2h(y_returned, device_data_y, 0, 0, width, 1, M, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+y_returned = np.zeros([M], dtype=np.float32)
+runner.memcpy_d2h(y_returned, device_data_y, 1, 0, 1, 1, M, streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
 
 runner.stop()
 
-host_y_expected = host_A.reshape(M,N)@host_x + host_b
 print(f"host_y_expected: {host_y_expected}")
 print(f"y_returned: {y_returned}")
-np.testing.assert_allclose(y_returned, np.tile(host_y_expected, width), atol=0.01, rtol=0)
+np.testing.assert_allclose(y_returned, host_y_expected, atol=0.01, rtol=0)
 print("Success!")
