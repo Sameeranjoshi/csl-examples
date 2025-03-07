1. Implement upper half of V cycle, host python side. 2. results match! But data is wrong, doing a pen and paper calculation    doesn't match.

benchmarks/amg_2d/amg.py
@@ -31,7 +31,7 @@ import math
 import utilities as ut
 
 def each_layer_solver_down(level, x_level, b_level, ml, setup_config, level_id, residual_host):
-    print(f"Solving on host layer: {level_id}")
+    print(f"Solving DOWN on host layer: {level_id}")
     A, R = level.A, level.R
     x = x_level[level_id]
     b = b_level[level_id]
@@ -45,6 +45,19 @@ def each_layer_solver_down(level, x_level, b_level, ml, setup_config, level_id, 
     
     residual_host.append(np.dot(r,r))
     return b_coarse, x_coarse
+
+def each_layer_solver_up(level, x_level, b_level, ml, setup_config, level_id, residual_host, x_lower_level):
+    print(f"Solving UP on host layer: {level_id}")
+    A, P = level.A, level.P
+    x = x_level[level_id]
+    b = b_level[level_id]
+
+    x += P @ x_lower_level  # Prolongation
+    ut.jacobi_csr(A, x, b, omega=get_omega_from_postsmoother(setup_config), iterations=get_iterations_from_postsmoother(setup_config))
+    
+    r = b - A @ x
+    residual_host.append(np.dot(r,r))
+    return x    
 
 # only V cycle, iterative version
 def AMG_test(ml, setup_config, x_level, b_level, solver, max_level=10, max_coarse=2, max_ite=1):    
@@ -83,8 +96,22 @@ def AMG_test(ml, setup_config, x_level, b_level, solver, max_level=10, max_coars
             x_coarsest[:] = solver(A_coarsest, b_coarsest)  # Call without extra kwargs
         print("After coarse solver:")
         debugprint(ml.levels, b_level, x_level)
+    
+        # Solve up
+        for i in reversed(range(len(ml.levels) - 1)):            
+            P = ml.levels[i].P
+            x_level[i] += P @ x_level[i+1]  # Prolongation
 
-    return b_coarsest, x_coarsest, A_coarsest
+            # Apply post-smoother
+            # ml.levels[i].postsmoother(ml.levels[i].A, x_level[i], b_level[i])
+            ut.jacobi_csr(ml.levels[i].A, x_level[i], b_level[i], omega=get_omega_from_postsmoother(setup_config), iterations=get_iterations_from_postsmoother(setup_config))
+    
+    
+    print("After REF final solver:")
+    debugprint(ml.levels, b_level, x_level)
+    # return b_coarsest, x_coarsest, A_coarsest # coarse check
+    return b_level[0], x_level[0]
+    
 # solve a linear system A * x = b
 # where A is a symmetric positive definite matrix
 # The AMG algorithm is from pyamg, https://github.com/pyamg/pyamg
@@ -323,7 +350,29 @@ def visualize(ml):
     plt.tight_layout()
     plt.savefig("sparse_matrix_visualization.png", dpi=300)
     plt.show()
+    
+def get_omega_from_postsmoother(smoothed_aggregation_solver_config):
+    # Extract omega from postsmoother configuration
+    postsmoother_config = smoothed_aggregation_solver_config.get('postsmoother', None)
+    if postsmoother_config and isinstance(postsmoother_config, tuple):
+        postsmoother_params = postsmoother_config[1]
+        omega = postsmoother_params.get('omega', None)
+        return omega 
+    else:
+        print("Postsmoother omega not found in configuration")
+        exit(1)
 
+def get_iterations_from_postsmoother(smoothed_aggregation_solver_config):
+    # Extract iterations from postsmoother configuration
+    postsmoother_config = smoothed_aggregation_solver_config.get('postsmoother', None)
+    if postsmoother_config and isinstance(postsmoother_config, tuple):
+        postsmoother_params = postsmoother_config[1]
+        iterations = postsmoother_params.get('iterations', None)
+        return iterations
+    else:
+        print("Postsmoother iterations not found in configuration")
+        exit(1)
+        
 def get_omega_from_presmoother(smoothed_aggregation_solver_config):
     # Extract omega from presmoother configuration
     presmoother_config = smoothed_aggregation_solver_config.get('presmoother', None)

benchmarks/amg_2d/run_amg.py
@@ -179,8 +179,7 @@ def host_calculations(v_cycle_data):
   ml_copy = copy.deepcopy(ml)
   setup_config_copy = copy.deepcopy(setup_config)
   solver_callable_host = amg.scipy_direct_solver
-  ref_b_coarsest, ref_x_coarsest, ref_A_coarsest = amg.AMG_test(ml_copy, setup_config_copy, x_level_copy, b_level_copy, max_level=10, max_coarse=2, max_ite=1, solver=solver_callable_host)
-    
+  ref_b_solution, ref_x_solution = amg.AMG_test(ml_copy, setup_config_copy, x_level_copy, b_level_copy, max_level=10, max_coarse=2, max_ite=1, solver=solver_callable_host)
   
   ## 
   if len(ml.levels) == 0:
@@ -204,14 +203,23 @@ def host_calculations(v_cycle_data):
   print("After coarse solver:")
   amg.debugprint(ml.levels, b_level, x_level)
   
+  for i in reversed(range(len(ml.levels) - 1)):    
+    level = ml.levels[i]  # current level
+    x_lower_level = x_level[i+1]  # this is data not array
+    x_current_updated = amg.each_layer_solver_up(level, x_level, b_level, ml, setup_config, i, residual_host, x_lower_level)
+    x_level[i] = x_current_updated
   
-  # check ref* and x_coarsest
-  assert np.allclose(ref_b_coarsest, b_coarsest, rtol=1e-5), "ref_b_coarsest do not match!"
-  assert np.allclose(ref_x_coarsest, x_coarsest, rtol=1e-5), "ref_x_coarsest do not match!"
-  assert np.allclose(A_coarsest.toarray(), ref_A_coarsest.toarray(), rtol=1e-6), "A_coarsest do not match!"
-  print("Success: b_coarsest, x_coarsest, A_coarsest match with reference")
+  b_solution, x_solution = b_level[0], x_level[0]
+  print("A = ", ml.levels[0].A.toarray())
+  print("After final solver:")
+  amg.debugprint(ml.levels, b_level, x_level)
+  # check ref* and x_solution
+  assert np.allclose(ref_b_solution, b_solution, rtol=1e-5), "ref_b_solution do not match!"
+  assert np.allclose(ref_x_solution, x_solution, rtol=1e-5), "ref_x_solution do not match!"
+  print("Success: b_solution, x_solution match with reference")
   
-  return b_coarsest, x_coarsest, residual_host
+
+  return b_solution, x_solution, residual_host
 
 def device_calculations(v_cycle_data):
   run_args, logs_dir = parse_args()
@@ -562,8 +570,8 @@ def main():
   print("############################################################")
   b_coarsest_host, x_coarsest_host, residual_host = host_calculations(v_cycle_data_host)
   print("HOST CALCULATIONS DONE")
-  print("\tb_coarsest Host:", b_coarsest_host.ravel())
-  print("\tx_coarsest Host:", x_coarsest_host.ravel())
+  print("\nb_solution_final Host:", b_coarsest_host.ravel())
+  print("\tx_solution_final Host:", x_coarsest_host.ravel())
   for i, residual in enumerate(residual_host):
       print(f"\tResidual Host [{i}]:", residual)
   print("############################################################")