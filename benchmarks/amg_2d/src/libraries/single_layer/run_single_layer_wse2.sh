9928430c7425a58d6f226f631c952cddfcdb0e11
sameeran <joshisameeran17@gmail.com>
2025-03-15 17:21:02 -0600

    design a single layer and it's dataflow, motivated from gemv-collectives

---
+++ benchmarks/amg_2d/src/libraries/single_layer/run_single_layer_wse2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+cslc --arch=wse2 ./single_layer_layout.csl --fabric-dims=22,12 --fabric-offsets=4,1 \
+--params=layer_rows_A:32,layer_cols_A:32 \
+--params=layer_start_x:0,layer_start_y:0,kernel_rows:4,kernel_cols:4,layer_index:0 \
+--memcpy --channels=1 -o out_single_layer
+cs_python single_layer_run.py --name out_single_layer
