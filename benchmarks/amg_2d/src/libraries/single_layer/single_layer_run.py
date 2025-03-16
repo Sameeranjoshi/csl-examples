56c013aa4684a5a16eed1451dc1bd65a9ec131b6
sameeran <joshisameeran17@gmail.com>
2025-03-16 01:51:56 -0600

    1. Implement remaining operators, for B next, answer off by a small    mistake.

--- benchmarks/amg_2d/src/libraries/single_layer/single_layer_run.py
+++ benchmarks/amg_2d/src/libraries/single_layer/single_layer_run.py
@@ -31,12 +31,13 @@ args = parser.parse_args()
 # Get params from compile metadata
 with open(f"{args.name}/out.json", encoding='utf-8') as json_file:
   compile_data = json.load(json_file)
-
-# Kernel rectangle and matrix dimensions
-kernel_rows = int(compile_data['params']['kernel_rows'])
-kernel_cols = int(compile_data['params']['kernel_cols'])
-matrix_rows = int(compile_data['params']['matrix_rows'])
-matrix_cols = int(compile_data['params']['matrix_cols'])
+# Kernel rectangle and matrix dimensions from compile parameters
+kernel_rows = int(compile_data['params']['layer_kernel_rows'])
+kernel_cols = int(compile_data['params']['layer_kernel_cols'])
+matrix_rows = int(compile_data['params']['layer_rows_A']) 
+matrix_cols = int(compile_data['params']['layer_cols_A'])
+restrict_rows = int(compile_data['params']['layer_rows_R'])
+restrict_cols = int(compile_data['params']['layer_cols_R'])
 
 # Create tensors for A, X, B.
 # Use a deterministic seed so that CI results are predictable
@@ -45,9 +46,12 @@ np.random.seed(seed=7)
 A = np.random.rand(matrix_rows, matrix_cols).astype(np.float32)
 X = np.random.rand(matrix_cols).astype(np.float32)
 B = np.random.rand(matrix_rows).astype(np.float32)
-
+# check if R_rows = layer_rows_A
+assert restrict_cols == matrix_rows, "restrict_cols must be equal to matrix_rows, example:(4,49) x (49,1) = (4,1)"
+R = np.random.rand(restrict_rows, restrict_cols).astype(np.float32)
 # Compute expected result
-y_expected = (A @ X) + B
+residual =  B - (A @ X)
+b_next_expected = R @ residual 
 
 # Specify path to ELF files, set up runner
 runner = SdkRuntime(args.name, cmaddr=args.cmaddr)
@@ -56,9 +60,10 @@ memcpy_dtype = MemcpyDataType.MEMCPY_32BIT
 memcpy_order = MemcpyOrder.ROW_MAJOR
 
 symbol_A = runner.get_id("A")
+symbol_R = runner.get_id("R")
 symbol_x = runner.get_id("x")
 symbol_b = runner.get_id("b")
-symbol_y = runner.get_id("y")
+symbol_b_next = runner.get_id("b_next") 
 
 runner.load()
 runner.run()
@@ -67,6 +72,9 @@ print("Copying data...")
 # Compute number of rows and cols of A on each PE
 per_pe_rows = matrix_rows // kernel_rows
 per_pe_cols = matrix_cols // kernel_cols
+
+per_pe_restrict_rows = restrict_rows // kernel_rows
+per_pe_restrict_cols = restrict_cols // kernel_cols
 
 # This transformation on A creates a flattened array so that the matrix can
 # be mapped onto the PEs using MemcpyOrder.ROW_MAJOR copy ordering.
@@ -102,9 +110,12 @@ per_pe_cols = matrix_cols // kernel_cols
 #   4. stacks them into a 4D array, with kernel_row as 0th dimension
 #   5. flattens into a 1D array
 data = np.stack(np.split(np.stack(np.split(A, kernel_cols, axis=1)), kernel_rows, axis=1)).ravel()
-
+data_R = np.stack(np.split(np.stack(np.split(R, kernel_cols, axis=1)), kernel_rows, axis=1)).ravel()
 # Copy flattened A array onto PEs
 runner.memcpy_h2d(symbol_A, data, 0, 0, kernel_cols, kernel_rows, per_pe_rows * per_pe_cols,
+                  streaming=False, data_type=memcpy_dtype, nonblock=False,
+                  order=memcpy_order)
+runner.memcpy_h2d(symbol_R, data_R, 0, 0, kernel_cols, kernel_rows, per_pe_restrict_rows * per_pe_restrict_cols,
                   streaming=False, data_type=memcpy_dtype, nonblock=False,
                   order=memcpy_order)
 
@@ -118,15 +129,15 @@ print("Launching kernel...")
 # Run the kernel
 runner.launch("main", nonblock=False)
 
-# Collect the result y from PE (kernel_cols-1,kernel_rows-1) and compare to expected
-y = np.zeros(matrix_rows, dtype=np.float32)
-runner.memcpy_d2h(y, symbol_y, kernel_cols-1, kernel_rows-1, 1, 1, matrix_rows,
+# Collect the result y from PE (kernel_cols-1, 0) and compare to expected, top-right corner of the PE grid
+b_next = np.zeros(restrict_rows, dtype=np.float32)
+runner.memcpy_d2h(b_next, symbol_b_next, kernel_cols-1, 0, 1, 1, restrict_rows,
                   streaming=False, data_type=memcpy_dtype, nonblock=False,
                   order=memcpy_order)
 runner.stop()
 print("Copied back result.")
 
-print("y calculated: ", y)
-print("y expected:   ", y_expected)
-np.testing.assert_allclose(y, y_expected, atol=0.01, rtol=0)
+print("b_next calculated: ", b_next)
+print("b_next expected:   ", b_next_expected)
+np.testing.assert_allclose(b_next, b_next_expected, atol=0.01, rtol=0)
 print("SUCCESS")
