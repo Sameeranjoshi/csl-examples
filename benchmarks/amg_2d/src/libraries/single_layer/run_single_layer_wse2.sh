2e623f90b6205498aad1404ddf19fa62e0a832b5
sameeran <joshisameeran17@gmail.com>
2025-03-15 18:43:27 -0600

    some more changes

--- benchmarks/amg_2d/src/libraries/single_layer/run_single_layer_wse2.sh
+++ benchmarks/amg_2d/src/libraries/single_layer/run_single_layer_wse2.sh
@@ -4,6 +4,6 @@ set -e
 
 cslc --arch=wse2 ./single_layer_layout.csl --fabric-dims=22,12 --fabric-offsets=4,1 \
 --params=layer_rows_A:32,layer_cols_A:32 \
---params=layer_start_x:0,layer_start_y:0,kernel_rows:4,kernel_cols:4,layer_index:0 \
+--params=layer_start_x:0,layer_start_y:0,layer_kernel_rows:4,layer_kernel_cols:4,layer_index:0 \
 --memcpy --channels=1 -o out_single_layer
 cs_python single_layer_run.py --name out_single_layer
