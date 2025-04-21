fdaea69f7fe0a8333bfdc6774fa8b14b0eefb5c6
sameeran <joshisameeran17@gmail.com>
2025-04-21 17:55:27 -0600

    dataflow works

--- tutorials/gemv-08-routes-3/commands_wse2.sh
+++ tutorials/gemv-08-routes-3/commands_wse2.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cslc --arch=wse2 ./layout.csl --fabric-dims=12,5 \
---fabric-offsets=4,1 --params=kernel_x_dim:4,kernel_y_dim:1,M:4,N:4 \
+cslc --arch=wse2 ./layout.csl --fabric-dims=18,5 \
+--fabric-offsets=4,1 --params=kernel_x_dim:5,kernel_y_dim:1,M:4,N:4 \
 -o out --memcpy --channels 1 --max-inlined-iterations=1000000
 cs_python run.py --name out
