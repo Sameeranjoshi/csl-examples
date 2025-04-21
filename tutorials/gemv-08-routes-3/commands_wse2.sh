5e36b9852bd31126084076fe9a52a878dd8a2c07
sameeran <joshisameeran17@gmail.com>
2025-04-20 21:31:40 -0600

    save commit working on datflow

--- tutorials/gemv-08-routes-3/commands_wse2.sh
+++ tutorials/gemv-08-routes-3/commands_wse2.sh
@@ -3,6 +3,6 @@
 set -e
 
 cslc --arch=wse2 ./layout.csl --fabric-dims=11,5 \
---fabric-offsets=4,1 --params=kernel_x_dim:4,kernel_y_dim:3,M:6,N:8 \
--o out --memcpy --channels 1
+--fabric-offsets=4,1 --params=kernel_x_dim:2,kernel_y_dim:1,M:4,N:4 \
+-o out --memcpy --channels 1 --max-inlined-iterations=1000000
 cs_python run.py --name out
--- tutorials/gemv-08-routes-3/run.py
+++ tutorials/gemv-08-routes-3/run.py
@@ -55,16 +55,20 @@ np.random.seed(seed=7)
 # mistake could be invisible. For example, if one entry is
 # not calculated correctly, it may still pass because the
 # error bound is too big. So rand() is better than arange().
-A = np.random.rand(M, N).astype(np.float32)
+A = np.full(shape=(M, N), fill_value=1.0, dtype=np.float32)
+A1 = np.full(shape=(M, N), fill_value=2.0, dtype=np.float32)  # 4x4
 x = np.full(shape=N, fill_value=1.0, dtype=np.float32)
-b = np.full(shape=M, fill_value=2.0, dtype=np.float32)
 
 # Calculate expected y
-y_expected = A@x + b
+y_expected = A@x
+print(f"y_expected: {y_expected}")
+y_expected1 = A1@x
+print(f"y_expected1: {y_expected1}")
+
 
 # Size of N dimension on each PE
-N_per_PE = N // kernel_x_dim
-M_per_PE = M // kernel_y_dim
+N_per_PE = N 
+M_per_PE = M
 
 # Construct a runner using SdkRuntime
 runner = SdkRuntime(args.name, cmaddr=args.cmaddr)
@@ -78,29 +82,34 @@ y_symbol = runner.get_id('y')
 runner.load()
 runner.run()
 
-# Copy b into y of PEs (0, 0) to (0, kernel_y_dim-1)
-runner.memcpy_h2d(y_symbol, b, 0, 0, 1, kernel_y_dim, M_per_PE, streaming=False,
-  order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
-
 # Copy chunks of A into all PEs
 # Each chunk on each PE is stored column major
-A_prepared = A.reshape(kernel_y_dim, M_per_PE, kernel_x_dim, N_per_PE).transpose(0, 2, 3, 1).ravel()
-runner.memcpy_h2d(A_symbol, A_prepared, 0, 0, kernel_x_dim, kernel_y_dim, M_per_PE*N_per_PE,
+# A0
+A_prepared = A.ravel()
+runner.memcpy_h2d(A_symbol, A_prepared, 0, 0, 1, 1, M_per_PE*N_per_PE,
+  streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT,
+  nonblock=False)
+# A1
+A1_prepared = A1.ravel()
+runner.memcpy_h2d(A_symbol, A1_prepared, 1, 0, 1, 1, M_per_PE*N_per_PE,
   streaming=False, order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT,
   nonblock=False)
 
 # Copy x into PEs (0, 0) and (kernel_x_dim-1, 0)
 # PE (0, 0) gets first N/2 elements; PE (1, 0) gets last N/2 elements
-runner.memcpy_h2d(x_symbol, x, 0, 0, kernel_x_dim, 1, N_per_PE, streaming=False,
+runner.memcpy_h2d(x_symbol, x, 0, 0, 1, 1, N_per_PE, streaming=False,
   order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
 
 # Launch the compute function on device
-runner.launch('compute', nonblock=False)
+runner.launch('host_facing_function', np.int16(0), nonblock=False)
 
-# Copy y back from PEs (kernel_x_dim-1, 0) and (kernel_x_dim-1, kernel_y_dim-1)
-y_result = np.zeros([M], dtype=np.float32)
-runner.memcpy_d2h(y_result, y_symbol, kernel_x_dim-1, 0, 1, kernel_y_dim, M_per_PE, streaming=False,
-  order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
+# Copy result back from last PE
+y_result = np.zeros(M, dtype=np.float32)
+runner.memcpy_d2h(
+    y_result, y_symbol, kernel_x_dim - 1, 0, 1, 1, M_per_PE,
+    streaming=False, order=MemcpyOrder.ROW_MAJOR,
+    data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False
+)
 
 # Stop the program
 runner.stop()
@@ -117,26 +126,11 @@ runner.stop()
 #   |y_result - y_expected|/(|A|*|x| + |b|) < tol
 #
 r = y_result - y_expected
-nrm_r = np.linalg.norm(r, np.inf)
-nrm_x = np.linalg.norm(x, np.inf)
-nrm_b = np.linalg.norm(b, np.inf)
-nrm_A = np.linalg.norm(A, np.inf)
-print(f"|y_result - y_expected| = {nrm_r}")
-print(f"|x| = {nrm_x}")
-print(f"|b| = {nrm_b}")
-print(f"|A| = {nrm_A}")
-
-relerr = nrm_r/(nrm_A*nrm_x + nrm_b)
-print(f"|y_result - y_expected|/(|A|*|x| + |b|) = {relerr}")
-
-assert relerr < 1.e-6, "norm-wise relative error is too big, something must be wrong"
-
 # The norm-wise estimate sometimes over-estimates too much, we can also
 # check the absolute error when the matrix is small.
 #
 # too large. If it fails (but norm-wise estimate can pass), try
 #   np.testing.assert_allclose(y_result/nrm_A, y_expected/nrm_A)
-if N < 10:
-  np.testing.assert_allclose(y_result, y_expected)
+np.testing.assert_allclose(y_result, y_expected)
 
 print("SUCCESS!")
