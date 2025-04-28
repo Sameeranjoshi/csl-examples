3761bb86254e8b3f39aa19a989ab48addd3052b9
sameeran <joshisameeran17@gmail.com>
2025-04-27 23:38:09 -0600

    prefix sum algo

---
+++ benchmarks/prefix_sum/run.py
@@ -0,0 +1,17 @@
+
+import argparse
+from cerebras.sdk.runtime.sdkruntimepybind import SdkRuntime, MemcpyDataType, MemcpyOrder  # pylint: disable=no-name-in-module
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', help='the test name')
+parser.add_argument('--cmaddr', help='IP:port for CS system')
+args = parser.parse_args()
+name = args.name
+
+# Simulate ELF files
+runner = SdkRuntime(args.name, cmaddr=args.cmaddr)
+
+runner.load()
+runner.run()
+runner.launch("init_data", nonblock=False)
+runner.stop()
