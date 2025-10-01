b2b8573d3a45b4e27c796032b13d1760f4a5eedc
sameeran <joshisameeran17@gmail.com>
2025-10-01 13:23:50 -0600

    wse3

--- benchmarks/non-unit-7pt/commands_wse3.sh
+++ benchmarks/non-unit-7pt/commands_wse3.sh
@@ -5,16 +5,16 @@ echo "\n------------------------------------------------------------------------
 echo "OPTIMIZED(Level L fine grained level)" >> results.txt.log
 echo "\n------------------------------------------------------------------------------------------------\n" >> results.txt.log
 # Test cases for 8x8x8 with BLOCK_SIZE=2,4,8
-cslc ./src/layout.csl --arch wse3 --fabric-dims=15,14 --fabric-offsets=4,1 \
---params=width:8,height:8,MAX_ZDIM:8 --params=BLOCK_SIZE:2 --params=C0_ID:0 \
---params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
---params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_8x8x8_block2 \
---memcpy --channels=1 --width-west-buf=0 --width-east-buf=0 --max-inlined-iterations=1000000
-cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out_8x8x8_block2 --channels=1 \
---width-west-buf=0 --width-east-buf=0 --zDim=8 --run-only
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=15,14 --fabric-offsets=4,1 \
+# --params=width:8,height:8,MAX_ZDIM:8 --params=BLOCK_SIZE:2 --params=C0_ID:0 \
+# --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
+# --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_8x8x8_block2 \
+# --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0 --max-inlined-iterations=1000000
+# cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out_8x8x8_block2 --channels=1 \
+# --width-west-buf=0 --width-east-buf=0 --zDim=8 --run-only
 # tail -n 8 sim.log >> results.txt.log
 
-# cslc ./src/layout.csl --arch wse2 --fabric-dims=15,14 --fabric-offsets=4,1 \
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=15,14 --fabric-offsets=4,1 \
 # --params=width:8,height:8,MAX_ZDIM:8 --params=BLOCK_SIZE:4 --params=C0_ID:0 \
 # --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
 # --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_8x8x8_block4 \
@@ -33,7 +33,7 @@ cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out --channels=1 \
 # tail -n 8 sim.log >> results.txt.log
 
 # Test cases for 4x4x4 with BLOCK_SIZE=2,4,8
-# cslc ./src/layout.csl --arch wse2 --fabric-dims=11,10 --fabric-offsets=4,1 \
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=11,10 --fabric-offsets=4,1 \
 # --params=width:4,height:4,MAX_ZDIM:4 --params=BLOCK_SIZE:2 --params=C0_ID:0 \
 # --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
 # --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_4x4x4_block2 \
@@ -42,7 +42,7 @@ cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out --channels=1 \
 # --width-west-buf=0 --width-east-buf=0 --zDim=4 --run-only
 # tail -n 8 sim.log >> results.txt.log
 
-# cslc ./src/layout.csl --arch wse2 --fabric-dims=11,10 --fabric-offsets=4,1 \
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=11,10 --fabric-offsets=4,1 \
 # --params=width:4,height:4,MAX_ZDIM:4 --params=BLOCK_SIZE:4 --params=C0_ID:0 \
 # --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
 # --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_4x4x4_block4 \
@@ -51,7 +51,7 @@ cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out --channels=1 \
 # --width-west-buf=0 --width-east-buf=0 --zDim=4 --run-only
 # tail -n 8 sim.log >> results.txt.log
 
-# cslc ./src/layout.csl --arch wse2 --fabric-dims=11,10 --fabric-offsets=4,1 \
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=11,10 --fabric-offsets=4,1 \
 # --params=width:4,height:4,MAX_ZDIM:4 --params=BLOCK_SIZE:8 --params=C0_ID:0 \
 # --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
 # --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_4x4x4_block8 \
@@ -61,7 +61,7 @@ cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out --channels=1 \
 # tail -n 8 sim.log >> results.txt.log
 
 # Test cases for 16x16x16 with BLOCK_SIZE=2,4,8
-# cslc ./src/layout.csl --arch wse2 --fabric-dims=27,26 --fabric-offsets=4,1 \
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=27,26 --fabric-offsets=4,1 \
 # --params=width:16,height:16,MAX_ZDIM:16 --params=BLOCK_SIZE:2 --params=C0_ID:0 \
 # --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
 # --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_16x16x16_block2 \
@@ -70,7 +70,7 @@ cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out --channels=1 \
 # --width-west-buf=0 --width-east-buf=0 --zDim=16 --run-only
 # tail -n 8 sim.log >> results.txt.log
 
-# cslc ./src/layout.csl --arch wse2 --fabric-dims=27,26 --fabric-offsets=4,1 \
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=27,26 --fabric-offsets=4,1 \
 # --params=width:16,height:16,MAX_ZDIM:16 --params=BLOCK_SIZE:4 --params=C0_ID:0 \
 # --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
 # --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_16x16x16_block4 \
@@ -79,7 +79,7 @@ cs_python ./run.py -m=8 -n=8 -k=8 --latestlink out --channels=1 \
 # --width-west-buf=0 --width-east-buf=0 --zDim=16 --run-only
 # tail -n 8 sim.log >> results.txt.log
 
-# cslc ./src/layout.csl --arch wse2 --fabric-dims=27,26 --fabric-offsets=4,1 \
+# cslc ./src/layout.csl --arch wse3 --fabric-dims=27,26 --fabric-offsets=4,1 \
 # --params=width:16,height:16,MAX_ZDIM:16 --params=BLOCK_SIZE:8 --params=C0_ID:0 \
 # --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 \
 # --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 -o=out_16x16x16_block8 \
