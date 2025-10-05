f643e384da3b705ae254a0e2302616e9520ccaab
sameeran <joshisameeran17@gmail.com>
2025-10-05 09:12:52 -0600

    run on h/w fails to compile, yet to add run code

--- benchmarks/gmg/csl_gmg/compile.py
+++ benchmarks/gmg/csl_gmg/compile.py
@@ -2,22 +2,29 @@ import sys
 import json
 import time
 from cerebras.sdk.client import SdkCompiler
+import os
 
 
-# cs_python ./run_gmg.py -m=8 -n=8 -k=8 --latestlink out --channels=1 \
-# --width-west-buf=0 --width-east-buf=0 --zDim=8 --run-only --levels=3
+# ABS_BENCHMARKS_ROOT = "/cloud_anl_homedir/sameeranj-cloud/AMGCerebras/benchmarks"
+# print CSL_IMPORT_PATH ENV variable
 
 out_path = "out"
-layout_file = "./src/layout_gmg.csl"
-ARGS=f"--arch=wse3 --fabric-dims=16,10 --fabric-offsets=4,1 --params=width:8,height:8,MAX_ZDIM:8 --params=BLOCK_SIZE:8 --params=C0_ID:0 --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 --memcpy --channels=1 --width-west-buf=0 --width-east-buf=0 --max-inlined-iterations=1000000"
+layout_file = "src/layout_gmg.csl"
+ARGS = (
+    f"--arch=wse3 --fabric-dims=762,1172 --fabric-offsets=4,1 "
+    f"--params=width:8,height:8,MAX_ZDIM:8 --params=BLOCK_SIZE:8 --params=C0_ID:0 --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 "
+    f"--memcpy --channels=1 --width-west-buf=0 --width-east-buf=0"
+    # f"--import-path '{IMPORT_PATHS}'"
+)
 
 # Instantiate compiler
-# 48 cores
-# 64GB memory
-with SdkCompiler(resource_cpu=48000, resource_mem=64<<30) as compiler:
+# 16 cores
+# 16GB memory = 16 << 30 = 16 *10^30(GB)
+# disable_version_check=True is added because of some internal bug, it converts the version into warning.
+with SdkCompiler(resource_cpu=16000, resource_mem=16<<30, disable_version_check=True) as compiler:
     # Launch compile job
     artifact_id = compiler.compile(
-        app_path=".",
+        app_path="src",
         csl_main=layout_file,
         options=ARGS,
         out_path=out_path,
--- benchmarks/gmg/csl_gmg/run_wse3.sh
+++ benchmarks/gmg/csl_gmg/run_wse3.sh
@@ -3,4 +3,4 @@
 set -e
 
 python compile.py
-python run_gmg.py -m=8 -n=8 -k=8 --latestlink out --channels=1 --width-west-buf=0 --width-east-buf=0 --zDim=8 --run-only --levels=3
+# python run_gmg.py -m=8 -n=8 -k=8 --latestlink out --channels=1 --width-west-buf=0 --width-east-buf=0 --zDim=8 --run-only --levels=3
