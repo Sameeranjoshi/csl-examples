c4bcf38145330502de14ee9257fe480f861e859a
Sameeran Joshi <joshisameeran17@gmail.com>
2025-10-14 22:17:37 +0000

    Restructure to make include paths clean, also compiles on device

--- benchmarks/gmg/csl_gmg/compile.py
+++ benchmarks/gmg/csl_gmg/compile.py
@@ -5,26 +5,20 @@ from cerebras.sdk.client import SdkCompiler
 import os
 
 
-# ABS_BENCHMARKS_ROOT = "/cloud_anl_homedir/sameeranj-cloud/AMGCerebras/benchmarks"
-# print CSL_IMPORT_PATH ENV variable
+out_path = "out_vcycle"
+layout_file = "./src/layout_gmg_vcycle.csl"
+ARGS=f"--arch=wse3 --fabric-dims=37,15 --fabric-offsets=17,1 --params=width:4,height:4,MAX_ZDIM:4,LEVELS:2 \
+    --params=BLOCK_SIZE:4 --params=C0_ID:0 --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 \
+    --params=C5_ID:5 --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 --memcpy --channels=4 \
+    --width-west-buf=13 --width-east-buf=13 --max-inlined-iterations=1000000"
 
-out_path = "out"
-layout_file = "src/layout_gmg.csl"
-ARGS = (
-    f"--arch=wse3 --fabric-dims=762,1172 --fabric-offsets=4,1 "
-    f"--params=width:8,height:8,MAX_ZDIM:8 --params=BLOCK_SIZE:8 --params=C0_ID:0 --params=C1_ID:1 --params=C2_ID:2 --params=C3_ID:3 --params=C4_ID:4 --params=C5_ID:5 --params=C6_ID:6 --params=C7_ID:7 --params=C8_ID:8 "
-    f"--memcpy --channels=1 --width-west-buf=0 --width-east-buf=0"
-    # f"--import-path '{IMPORT_PATHS}'"
-)
 
+print("Start compiling: "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), flush=True)
 # Instantiate compiler
-# 16 cores
-# 16GB memory = 16 << 30 = 16 *10^30(GB)
-# disable_version_check=True is added because of some internal bug, it converts the version into warning.
-with SdkCompiler(resource_cpu=16000, resource_mem=16<<30, disable_version_check=True) as compiler:
+with SdkCompiler(resource_cpu=48000, resource_mem=64<<30, disable_version_check=True) as compiler:
     # Launch compile job
     artifact_id = compiler.compile(
-        app_path="src",
+        app_path=".",
         csl_main=layout_file,
         options=ARGS,
         out_path=out_path,
--- benchmarks/gmg/csl_gmg/run_wse3.sh
+++ benchmarks/gmg/csl_gmg/run_wse3.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-
+# if [ -z "$1" ]; then
+#     echo "Error: Simulator value must be specified as the first argument (true or false)."
+#     exit 1
+# fi
+# simulator=$1
 python compile.py
 # python run_gmg.py -m=8 -n=8 -k=8 --latestlink out --channels=1 --width-west-buf=0 --width-east-buf=0 --zDim=8 --run-only --levels=3
