Fix convergence formula, bug norm_2 vs norm_inf

benchmarks/gmg/csl_gmg/python_gmg/gmgoscar.py
@@ -163,7 +163,7 @@ class SimpleGMG:
         # Simple parameters
         self.omega = 2.0/3.0  # Jacobi relaxation parameter
 
-        self.nrm_b = np.linalg.norm(self.grids[0]['f'].ravel(), 2)
+        self.nrm_b = np.linalg.norm(self.grids[0]['f'].ravel(), np.inf)
         self.abs_tolerance = abs_tolerance
         self.rel_tolerance = self.abs_tolerance * self.nrm_b
         self.ALPHA = -6.0